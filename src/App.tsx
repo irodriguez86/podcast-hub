1622f36063e31eef2f67d4777cd6e920d5087e56
Israel Sanchez <isra.mullen86@gmail.com>
2023-04-05 21:40:06 +0200

    Create the podcast detail route

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Hub from './components/Hub'
 import PodcastDetail from './components/PodcastDetail'
+import { PodcastProvider } from './context/PodcastContext'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Hub />} />
-        <Route path="/podcast" element={<PodcastDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <PodcastProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Hub />}></Route>
+          <Route path="/podcast/:id" element={<PodcastDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </PodcastProvider>
   )
 }
 
