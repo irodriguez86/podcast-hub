088fbdaca5bc8571e4da2b1a1beb85ec0a88c531
Israel Sanchez <isra.mullen86@gmail.com>
2023-04-28 12:16:05 +0200

    Memoize filter podcast function

--- src/views/Hub/Hub.tsx
+++ src/views/Hub/Hub.tsx
@@ -1,24 +1,27 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useCallback, useContext, useState } from 'react'
 import Podcast from '../../components/Podcast/Podcast'
 import './Hub.css'
 import PodcastContext from '../../context/PodcastContext'
-import { useFetchPodcastList } from '../../hooks/useFetchPodcastList'
+import { PodcastInfo, useFetchPodcastList } from '../../hooks/useFetchPodcastList'
 
 export const Hub: React.FC = () => {
   const { podcastList = [] } = useContext(PodcastContext)
   const [searchQuery, setSearchQuery] = useState('')
-  //TODO: check if we can do this only when podcastlist is null
-  useFetchPodcastList()
+  
+  const filterPodcasts = useCallback((list: PodcastInfo[] | null) => {
+    if (!list) {
+      return undefined;
+    }
+    return list.filter((podcast) => {
+      const nameMatch = podcast.name.toLowerCase().includes(searchQuery.toLowerCase())
+      const artistMatch = podcast.artist.toLowerCase().includes(searchQuery.toLowerCase())
+      return nameMatch || artistMatch
+    });
+  }, [searchQuery])
 
-  const filteredPodcasts = podcastList?.filter((podcast) => {
-    const nameMatch = podcast.name
-      .toLowerCase()
-      .includes(searchQuery.toLowerCase())
-    const artistMatch = podcast.artist
-      .toLowerCase()
-      .includes(searchQuery.toLowerCase())
-    return nameMatch || artistMatch
-  })
+  useFetchPodcastList()
+  
+  const filteredPodcasts = filterPodcasts(podcastList)
 
   return (
     <div className="main-cointainer">
@@ -35,8 +38,7 @@ export const Hub: React.FC = () => {
         />
       </div>
       <div className="podcast-hub-container">
-        {filteredPodcasts &&
-          filteredPodcasts.map((podcast) => {
+        {filteredPodcasts && filteredPodcasts.map((podcast) => {
             return (
               <Podcast
                 key={podcast.id}
